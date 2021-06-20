8f4cd3d9f1e1dca7321e227f78dd3e32bf3528e7
AdamMiltonBarker <adammiltonbarker@eu.techbubbletechnologies.com>
2021-06-20 03:09:07 +0200

    1.0.0 Fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,9 +7,11 @@ read -p "? This script will install the HIASBCH MQTT Blockchain Agent on your de
 if [ "$cmsg" = "Y" -o "$cmsg" = "y" ]; then
     echo "- Installing HIASBCH MQTT Blockchain Agent"
 	pip3 install --user flask
+	pip3 install --user gevent
 	pip3 install --user psutil
 	pip3 install --user requests
 	pip3 install --user web3
+	pip3 install --user gevent
     echo "- HIASBCH MQTT Blockchain Agent installed!"
 else
     echo $FMSG;
