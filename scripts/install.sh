241016eaf1a3d2b1a8daec5279861ebdd474aab9
AdamMiltonBarker <adammiltonbarker@eu.techbubbletechnologies.com>
2021-08-01 18:26:31 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-FMSG="- HIAS MQTT IoT Agent installation terminated"
+FMSG="- HIASBCH MQTT Blockchain Agent installation terminated"
 
-echo "This script will install the HIAS MQTT IoT Agent on your device."
+echo "This script will install the HIASBCH MQTT Blockchain Agent on your device."
 read -p "Proceed (y/n)? " proceed
 
 if [ "$proceed" = "Y" -o "$proceed" = "y" ]; then
-    echo "- Installing HIAS MQTT IoT Agent"
+    echo "- Installing HIASBCH MQTT Blockchain Agent"
     pip3 install --user bcrypt
     pip3 install --user flask
     pip3 install --user paho-mqtt
     pip3 install --user psutil
     pip3 install --user requests
     pip3 install --user web3
-    echo "- HIAS MQTT IoT Agent installed!"
+    echo "- HIASBCH MQTT Blockchain Agent installed!"
 else
     echo $FMSG;
     exit
